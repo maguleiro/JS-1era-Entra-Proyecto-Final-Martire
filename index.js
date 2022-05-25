cff94170233edb5c77784f95316a638c4602b7b3
Lucas Martire <marlumon125@gmail.com>
2022-05-24 21:36:55 -0300

    primera entrega proyecto

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
+let nombre = prompt("¿Cual es tu nombre?");
+const plata = 1000;
 
-let contador = 0;
-let usuario =[];
+function saludar(){
 
-while(contador <4)
-{
-	let nombre = prompt("Ingrese su nombre");
-	let apellido = prompt("Ingrese su apellido");
-	let email = prompt("Ingrese su email");
-	let contrasenia = prompt("Ingrese su contrasenia");
-	usuario.push(nombre, apellido, email, contrasenia);
-	contador++;
-	break;
-
+	respuesta = prompt("Hola " + nombre + " ¿Como fue tu dia?");
+	if (respuesta == "bien"){
+		alert("¡Me alegro!")
+	} else{
+		alert("Una pena...")
+	}
 }
-console.log("Los datos del usuario son:")
-console.log(usuario);
 
+saludar()
 
+let fernet = 500;
+let jagger = 850;
+let gin = 450;
+let birra = 250;
+let vodka = 1500;
+
+function bebida (){
+
+	respuesta = prompt (`¿Que te gustaria tomar?
+						-Fernet $500
+						-Jaggermeister $850
+						-Gin Tonic $450
+						-Birra $250
+						-Vodka $1500`)
+	
+	if (respuesta == "fernet"){
+		let resta = plata - fernet;
+		alert ("Muchas gracias, tu vuelto es " + resta)
+	}
+	if (respuesta == "jaggermeister"){
+		let resta = plata - jagger;
+		alert ("Muchas gracias, tu vuelto es " + resta)
+	}
+	if (respuesta == "gin"){
+		let resta = plata - gin;
+		alert ("Muchas gracias, tu vuelto es " + resta)
+	}
+	if (respuesta == "birra"){
+		let resta = plata - birra;
+		alert ("Muchas gracias, tu vuelto es " + resta)
+	}
+	if (respuesta == "vodka"){
+		let resta = vodka - plata;
+		alert ("No te alcanza te faltan " + resta)
+	}
+}
+
+bebida()
